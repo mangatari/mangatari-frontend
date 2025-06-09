6d4200f2b88f29ed172991dc0b41b21e01f013f8
“nekosaege” <nekofreiheit@gmx.de>
2025-06-09 08:51:50 +0200

    feat: Add pixel-footer styles to App.css and refactor Footer component to use CSS class

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,9 @@
 import React from "react";
+import "../App.css"; 
 
 function Footer() {
   return (
-    <footer
-      style={{
-        textAlign: "center",
-        padding: "1rem",
-        borderTop: "1px solid #ccc",
-        marginTop: "2rem",
-        fontSize: "0.9rem",
-        color: "#666",
-      }}
-    >
+    <footer className="pixel-footer">
       Loop Troop 2025 Ironhack
     </footer>
   );
